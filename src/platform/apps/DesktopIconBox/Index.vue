7bc6d49e3c6af814e224655cda14d04fb2693f12
OXOYO <zmn2007.hi@163.com>
2018-01-01 16:19:09 +0800

    添加DesktopIcon拖拽功能、动态计算位置数据。

--- src/platform/apps/DesktopIconBox/Index.vue
+++ src/platform/apps/DesktopIconBox/Index.vue
@@ -18,7 +18,7 @@
   <div class="app-desktop-icon-box">
     <!-- 应用列表 -->
     <DesktopIcon
-      v-for="item in appData.iconList"
+      v-for="item in iconList"
       :key="item.app.id"
       :info="item"
       :showTitle="appData.showTitle"
@@ -45,6 +45,48 @@
           }
         },
         required: true
+      }
+    },
+    computed: {
+      iconList: function () {
+        let _t = this
+        let tmpArr = []
+        // 处理宽高
+        let height = document.body.clientHeight
+        let width = document.body.clientWidth
+        // 每个图标宽高80px
+        let itemWidthHeight = 80
+        let xNum = width % itemWidthHeight
+        let yNum = height % itemWidthHeight
+        // 列计数器
+        let col = 0
+        // 行计数器
+        let row = 0
+        // FIXME 需要减掉 taskBar 的高或宽，减高或减宽取决于taskBar的上右下左位置
+        for (let i in _t.appData.iconList) {
+          let item = _t.appData.iconList[i]
+          if (!item.app.style) {
+            item.app.style = {}
+          }
+          if (row <= yNum) {
+            item.app.style = {
+              left: col * itemWidthHeight + 'px',
+              top: row * itemWidthHeight + 'px'
+            }
+            row++
+          } else {
+            row = 0
+            if (col < xNum) {
+              col++
+            } else {
+              console.log('Col needs less than xNum')
+            }
+          }
+          console.log('item.app.style', item.app.title, item.app.style)
+          tmpArr[i] = item
+        }
+        console.log('tmpArr', tmpArr)
+        return tmpArr
       }
     }
   }
