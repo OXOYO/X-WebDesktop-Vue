66ce7ef202406dfd8f19f33d356929e3e9bb37aa
OXOYO <zmn2007.hi@163.com>
2018-01-08 17:41:23 +0800

    修改端口号。

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8090,
+    port: 8899,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
