0c96a3ff29245b6c1758782b6d77f2c9342a0d46
OXOYO <zmn2007.hi@163.com>
2018-01-08 17:41:53 +0800

    完善登录、登出用户验证。

---
+++ src/platform/apps/Admin/store/actions.js
@@ -0,0 +1,13 @@
+/**
+ * Created by OXOYO on 2017/12/5.
+ */
+
+import Api from '../api'
+
+export default {
+  // 获取bing壁纸
+  'user/BaseInfo': async ({ commit }, payload) => {
+    let res = await Api.user.getBaseInfo(payload)
+    return res
+  }
+}
