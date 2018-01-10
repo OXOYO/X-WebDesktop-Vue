92997fbf93d2125265f1c385f2722dda0f56fb97
OXOYO <zmn2007.hi@163.com>
2018-01-10 09:59:58 +0800

    修改密码显示、隐藏图标。

--- src/platform/apps/Login/Index.vue
+++ src/platform/apps/Login/Index.vue
@@ -75,7 +75,7 @@
             <Form-item prop="password">
               <Input :type="passwordInputType" v-model="formData.password" placeholder="请输入密码" @on-enter="handleSignIn">
               <Icon type="ios-locked-outline" slot="prepend" style="font-size: 16px;"></Icon>
-              <Button slot="append" :icon="passwordInputType === 'password' ? 'eye' : 'eye-disabled'" style="font-size: 16px; line-height: 1;" @click="showPassword"></Button>
+              <Button slot="append" :icon="passwordInputType === 'password' ? 'eye-disabled' : 'eye'" style="font-size: 16px; line-height: 1;" @click="showPassword"></Button>
               </Input>
             </Form-item>
           </Form>
