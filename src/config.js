6b028d51f47ab8c577bf9759b449175546038e85
OXOYO <zmn2007.hi@163.com>
2018-02-12 17:13:47 +0800

    更新线上接口地址。

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@
 // 服务配置
 export const Serv = {
   development: '//localhost:3000/api/v2.0.0',
-  production: '//localhost:3000/api/v2.0.0'
+  production: '//x-webdesktop-api-koa.herokuapp.com/api/v2.0.0'
 }
 
 // cookie相关配置
@@ -29,6 +29,6 @@ export const System = {
   name: 'X-WebDesktop-Vue',
   title: 'X-WebDesktop-Vue',
   subtitle: 'X-WebDesktop-Vue',
-  logo: '/static/logo.png',
+  logo: './static/logo.png',
   copyright: '© 2017'
 }
