7feb0769c084b4b381c8a4a26704b71ead23bdc3
OXOYO <zmn2007.hi@163.com>
2018-04-21 00:55:56 +0800

    优化样式，禁用选择。

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@
     margin: 0;
     padding: 0;
     overflow: hidden;
+    cursor: default;
+    user-select: none;
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
