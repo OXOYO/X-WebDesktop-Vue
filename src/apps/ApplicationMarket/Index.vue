7620b34314adc1a52d61bc782e35e39def3d3108
OXOYO <zmn2007.hi@163.com>
2018-04-20 17:23:46 +0800

    新增应用市场APP。

---
+++ src/apps/ApplicationMarket/Index.vue
@@ -0,0 +1,50 @@
+/**
+* Created by OXOYO on 2018/4/20.
+*
+*/
+
+<style scoped lang="less" rel="stylesheet/less">
+  .app-main {
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+  }
+</style>
+
+<template>
+  <div class="app-main">
+    <MainPage></MainPage>
+  </div>
+</template>
+
+<script>
+  import MainPage from './pages/MainPage.vue'
+  import Store from './store'
+
+  export default {
+    name: 'ApplicationMarket',
+    components: {
+      MainPage
+    },
+    data () {
+      return {
+        isLoading: true
+      }
+    },
+    created: function () {
+      let _t = this
+
+      setTimeout(function () {
+        _t.isLoading = false
+      }, 200)
+      // 将store注册到apps下
+      _t.$store.registerModule(['Apps', Store.moduleName], Store.store)
+    },
+    destroyed: function () {
+      let _t = this
+      // 卸载store
+      _t.$store.unregisterModule(['Apps', Store.moduleName])
+    }
+  }
+</script>
+
---
+++ src/apps/ApplicationMarket/store/actions.js
@@ -0,0 +1,15 @@
+/**
+ * Created by OXOYO on 2018/4/20.
+ */
+
+// 导入api
+import Api from '../api'
+
+export default {
+  // 获取账号列表
+  'application/list': async ({ commit }, payload) => {
+    // 调接口
+    let res = await Api.getApplicationList(payload)
+    return res
+  }
+}
