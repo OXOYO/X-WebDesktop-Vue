444b3c26a5741c9ea34585164994daf61d3fdfaf
OXOYO <zmn2007.hi@163.com>
2018-04-23 15:07:22 +0800

    完善应用市场界面、接口调用。

--- src/apps/ApplicationMarket/api.js
+++ src/apps/ApplicationMarket/api.js
@@ -5,6 +5,13 @@
 import Vue from 'vue'
 
 export default {
+  // 获取应用分类列表
+  getCategoryList: async (data) => {
+    let res = await Vue.prototype.$http.get('/ApplicationMarket/category/list', {
+      params: data
+    })
+    return res
+  },
   // 获取应用列表
   getApplicationList: async (data) => {
     let res = await Vue.prototype.$http.get('/ApplicationMarket/application/list', {
