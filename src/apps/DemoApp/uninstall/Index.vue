de8eb536e287976c6164567a29cc40122f001071
OXOYO <zmn2007.hi@163.com>
2018-04-26 15:45:35 +0800

    添加示例应用安装、卸载自定义界面。

--- src/apps/DemoApp/uninstall/Index.vue
+++ src/apps/DemoApp/uninstall/Index.vue
@@ -1,7 +1,7 @@
 /**
-* Created by OXOYO on 2018/4/24.
+* Created by yangfan9244 on 2018/4/26.
 *
-* 自定义安装界面
+* 自定义卸载界面
 */
 
 <style scoped lang="less" rel="stylesheet/less">
@@ -46,6 +46,8 @@
       }
       .complete {
         display: inline-block;
+        width: 100%;
+        text-align: center;
         margin-top: 10px;
       }
       .button {
@@ -58,27 +60,27 @@
 <template>
   <div class="app-block">
     <div class="block-header">
-      <div class="title">{{ info.app.title }}</div>
+      <div class="title">{{ info.app_title || info.config.app.title }}</div>
     </div>
     <div class="block-body">
       <div class="logo">
-        <img :src="info.app.icon" :alt="info.app.title">
+        <img :src="info.config.app.icon" :alt="info.app_title || info.config.app.title">
       </div>
       <div class="loading" v-show="isLoading">
         <Spin fix>
           <Icon class="loading-icon" type="load-c" size=18></Icon>
-          <div class="loading-text">安装中...</div>
+          <div class="loading-text">卸载中...</div>
         </Spin>
       </div>
-      <div class="complete" v-show="isComplete">{{ isSuccess ? '安装成功！' : '安装失败！' }}</div>
-      <Button class="button" type="primary" v-show="!isLoading" @click="handleInstall">立即安装</Button>
+      <div class="complete" v-show="isComplete">{{ isSuccess ? '卸载成功！' : '卸载失败！' }}</div>
+      <Button class="button" type="primary" v-show="!isLoading" @click="handleUninstall">立即卸载</Button>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Install',
+    name: 'Uninstall',
     props: {
       info: {
         type: Object
@@ -95,13 +97,12 @@
       }
     },
     methods: {
-      handleInstall: function () {
+      handleUninstall: function () {
         let _t = this
-        console.log('handleInstall')
         _t.isLoading = true
         // 广播事件 触发window事件 open
-        _t.$utils.bus.$emit('platform/application/install', {
-          action: 'doInstall',
+        _t.$utils.bus.$emit('platform/application/uninstall', {
+          action: 'doUninstall',
           data: {
             appInfo: _t.info,
             callback: (isSuccess) => {
@@ -115,3 +116,4 @@
     }
   }
 </script>
+
