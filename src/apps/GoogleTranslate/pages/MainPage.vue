03909f20ed7ee0f65898087c4d9cdaa945e7813d
OXOYO <zmn2007.hi@163.com>
2018-04-30 21:28:37 +0800

    新增谷歌翻译应用。

---
+++ src/apps/GoogleTranslate/pages/MainPage.vue
@@ -0,0 +1,99 @@
+/**
+* Created by OXOYO on 2018/4/23.
+*
+*/
+
+<style scoped lang="less" rel="stylesheet/less">
+  .main-page {
+    width: 100%;
+    height: 100%;
+    display: inline-block;
+    text-align: center;
+    overflow: hidden;
+
+    .iframe-box {
+      margin: 0 auto;
+      width: 100%;
+      height: 100%;
+    }
+  }
+</style>
+
+<template>
+  <div class="main-page">
+    <iframe
+      class="iframe-box"
+      name="iframe-box"
+      :src="appPath"
+      :width="appWidth"
+      :height="appHeight"
+      frameborder="0"
+      marginwidth="0"
+      marginheight="0"
+      scrolling="no"
+      target="_top"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MainPage',
+    data () {
+      return {
+        appUrl: '//translate.google.cn/m/translate',
+        appPath: null,
+        appWidth: 0,
+        appHeight: 0
+      }
+    },
+    props: {
+      info: {
+        type: Object
+      }
+    },
+    watch: {
+      info: {
+        handler: function () {
+          let _t = this
+          // 初始化窗口
+          _t.init()
+        },
+        deep: true
+      }
+    },
+    methods: {
+      // 初始化
+      init: function () {
+        let _t = this
+        // 加载应用
+        _t.loadApp()
+      },
+      // 加载应用
+      loadApp: function () {
+        let _t = this
+        _t.$nextTick(function () {
+          _t.appWidth = parseInt(_t.$el.offsetWidth)
+          _t.appHeight = parseInt(_t.$el.offsetHeight)
+          _t.appPath = _t.appUrl
+          document.body.onbeforeunload = function (event) {
+            if (!window.event) {
+              event.returnValue = false
+            } else {
+              window.event.returnValue = false
+            }
+          }
+        })
+      }
+    },
+    created: function () {
+      let _t = this
+      // 初始化窗口
+      _t.init()
+    },
+    beforeDestroy: function () {
+      document.body.onbeforeunload = null
+    }
+  }
+</script>
