b450cfe84f2e31fbb16c03488e165dfe56f395a1
OXOYO <zmn2007.hi@163.com>
2018-05-02 22:20:43 +0800

    完善应用市场，支持用户应用卸载。

--- src/apps/ApplicationMarket/pages/MainPage.vue
+++ src/apps/ApplicationMarket/pages/MainPage.vue
@@ -11,13 +11,13 @@
     display: flex;
 
     .category-block {
-      flex: 0 0 200px;
+      flex: 0 0 120px;
       height: 100%;
       border-right: 1px solid #dddddd;
 
       .category-header {
-        height: 30px;
-        line-height: 30px;
+        height: 40px;
+        line-height: 40px;
         padding-left: 20px;
         border-bottom: 1px solid #dddddd;
       }
@@ -28,12 +28,66 @@
         .category-list {
 
           .list-item {
-            padding: 5px 0 5px 30px;
+            padding: 10px 0 10px 20px;
             border-bottom: 1px solid #ffffff;
 
             &:hover,
             &.active {
-              background: #cfe3fd;
+              background: #0CCC6C;
+
+              .item-icon,
+              .item-title {
+                color: #ffffff;
+              }
+            }
+
+            .item-icon {
+              display: inline-block;
+              width: 20px;
+              height: 20px;
+              line-height: 20px;
+              vertical-align: middle;
+              color: #BFBFBF;
+            }
+            .item-title {
+              display: inline-block;
+              height: 20px;
+              line-height: 20px;
+              vertical-align: middle;
+            }
+          }
+        }
+      }
+    }
+    .app-list-block {
+      flex: 1 1 auto;
+      height: 100%;
+
+      .list-header {
+        display: inline-block;
+        width: 100%;
+
+        .board-block {
+          display: flex;
+          width: 100%;
+          height: 40px;
+          line-height: 40px;
+          border-bottom: 1px solid #dddddd;
+
+          .board-item {
+            flex: 1 1 auto;
+            height: 40px;
+            line-height: 40px;
+            text-align: center;
+
+            &:hover,
+            &.active {
+              background: #0CCC6C;
+
+              .item-icon,
+              .item-title {
+                color: #ffffff;
+              }
             }
 
             .item-icon {
@@ -52,21 +106,13 @@
           }
         }
       }
-    }
-    .app-list-block {
-      flex: 1 1 auto;
-      height: 100%;
-
-      .list-header {
-        display: none;
-      }
       .list-body {
         display: flex;
         flex-wrap: wrap;
         justify-content: flex-start;
         align-content: flex-start;
         width: 100%;
-        height: 100%;
+        /*height: 100%;*/
 
         .list-item {
           flex: 0 0 auto;
@@ -148,19 +194,31 @@
     </div>
     <div class="app-list-block">
       <!-- FIXME 可以考虑分类下还存在子分类的情况 -->
-      <div class="list-header"></div>
+      <div class="list-header">
+        <div class="board-block">
+          <div
+            v-for="item in boardList"
+            :class="{ 'board-item': true, 'active': currentBoard.name === item.name }"
+            :key="item.name"
+            @click="handleBoardTrigger(item)"
+          >
+            <Icon v-if="item.icon" class="item-icon" :type="item.icon"></Icon>
+            <div class="item-title">{{ item.title }}</div>
+          </div>
+        </div>
+      </div>
       <div class="list-body">
         <div
           class="list-item"
           v-for="item in applicationList"
-          :key="item.name"
+          :key="item.name || item.app_name"
         >
           <div class="item-logo">
-            <img :src="item.config.app.icon || appIcon" :alt="item.title">
+            <img :src="item.config.app.icon || appIcon" :alt="item.title || item.app_title">
           </div>
           <div class="item-content">
-            <div class="item-title">{{ item.title }}</div>
-            <div class="item-install" @click.stop.prevent="handleApplicationInstall(item)">安装</div>
+            <div class="item-title">{{ item.title || item.app_title}}</div>
+            <div class="item-install" @click.stop.prevent="handleAction(item, currentBoard.action.name)">{{ currentBoard.action.text }}</div>
           </div>
         </div>
         <NoData :show="!applicationList.length">该分类下暂无应用！</NoData>
@@ -181,7 +239,29 @@
         // 应用列表
         applicationList: [],
         // 当前激活的分类
-        currentCategory: null
+        currentCategory: null,
+        boardList: [
+          {
+            name: 'treasury',
+            icon: '',
+            title: '宝库',
+            action: {
+              name: 'install',
+              text: '安装'
+            }
+          },
+          {
+            name: 'uninstall',
+            icon: '',
+            title: '卸载',
+            action: {
+              name: 'uninstall',
+              text: '卸载'
+            }
+          }
+        ],
+        // 当前激活的board
+        currentBoard: null
       }
     },
     computed: {
@@ -195,6 +275,7 @@
     methods: {
       init: async function () {
         let _t = this
+        _t.currentBoard = _t.boardList[0]
         // 获取应用分类列表
         await _t.getCategoryList()
         // 依据当前激活的分类获取应用列表
@@ -229,10 +310,27 @@
       // 依据当前激活的分类获取应用列表
       getApplicationList: async function () {
         let _t = this
+        let path = ''
+        let payload = {}
+        // 获取当前board下的应用列表
+        switch (_t.currentBoard.name) {
+          case 'treasury':
+            // 获取应用列表
+            path = 'Apps/ApplicationMarket/application/list'
+            payload = {
+              category: _t.currentCategory ? _t.currentCategory.id : ''
+            }
+            break
+          case 'uninstall':
+            // 获取用户应用数据
+            path = _t.$utils.store.getType('Admin/user/application/list', 'Platform')
+            payload = {
+              app_category: _t.currentCategory ? _t.currentCategory.id : ''
+            }
+            break
+        }
         // 分发action，调接口
-        let res = await _t.$store.dispatch('Apps/ApplicationMarket/application/list', {
-          category: _t.currentCategory ? _t.currentCategory.id : ''
-        })
+        let res = await _t.$store.dispatch(path, payload)
         if (!res) {
           _t.$Message.error('查询应用列表失败！')
           return
@@ -259,55 +357,90 @@
         // 获取当前分类下的应用列表
         _t.getApplicationList()
       },
-      // 处理应用安装
-      handleApplicationInstall: function (appInfo) {
+      // 处理应用安装/卸载
+      handleAction: function (appInfo, action) {
         let _t = this
-        // 安装信息
-        let installInfo = {}
-        let iconList = [..._t.appData.iconList]
-        // 查找单个索引
-        let findAppIndex = function (iconList, condition) {
-          return iconList.findIndex((item) => {
-            return condition(item)
-          })
+        // 处理安装
+        let handleInstall = function () {
+          // 安装信息
+          let installInfo = {}
+          let iconList = [..._t.appData.iconList]
+          // 查找单个索引
+          let findAppIndex = function (iconList, condition) {
+            return iconList.findIndex((item) => {
+              return condition(item)
+            })
+          }
+          let currentAppIndex = findAppIndex(iconList, (item) => item.config.app.name === appInfo.config.app.name)
+          if (currentAppIndex < 0) {
+            installInfo = {
+              // 解构应用基础配置
+              ...appInfo,
+              config: {
+                ...appInfo.config,
+                // 解构应用安装配置
+                ...appInfo.config.install
+              },
+              // 应用ID
+              appID: appInfo.id,
+              // 赋值当前操作为 install
+              action: 'install',
+              // 是否已安装过
+              installed: false
+            }
+          } else {
+            let currentApp = iconList[currentAppIndex]
+            installInfo = {
+              // 解构应用基础配置
+              ...currentApp,
+              config: {
+                ...currentApp.config,
+                // 解构应用安装配置
+                ...appInfo.config.install
+              },
+              // 应用ID
+              appID: appInfo.id,
+              // 赋值当前操作为 install
+              action: 'install',
+              // 是否已安装过
+              installed: true
+            }
+          }
+          // 调用安装工具，打开安装界面
+          _t.$utils.install(_t, installInfo)
         }
-        let currentAppIndex = findAppIndex(iconList, (item) => item.config.app.name === appInfo.config.app.name)
-        if (currentAppIndex < 0) {
-          installInfo = {
+        // 处理卸载
+        let handleUninstall = function () {
+          // 调用安装工具，打开安装界面
+          _t.$utils.install(_t, {
             // 解构应用基础配置
             ...appInfo,
             config: {
               ...appInfo.config,
-              // 解构应用安装配置
-              ...appInfo.config.install
+              // 解构应用卸载配置
+              ...appInfo.config.uninstall
             },
-            // 应用ID
-            appID: appInfo.id,
-            // 赋值当前操作为 install
-            action: 'install',
-            // 是否已安装过
-            installed: false
-          }
-        } else {
-          let currentApp = iconList[currentAppIndex]
-          installInfo = {
-            // 解构应用基础配置
-            ...currentApp,
-            config: {
-              ...currentApp.config,
-              // 解构应用安装配置
-              ...appInfo.config.install
-            },
-            // 应用ID
-            appID: appInfo.id,
-            // 赋值当前操作为 install
-            action: 'install',
+            // 赋值当前操作为 uninstall
+            action: 'uninstall',
             // 是否已安装过
             installed: true
-          }
+          })
         }
-        // 调用安装工具，打开安装界面
-        _t.$utils.install(_t, installInfo)
+        switch (action) {
+          case 'install':
+            handleInstall()
+            break
+          case 'uninstall':
+            handleUninstall()
+            break
+        }
+      },
+      // 处理board点击事件
+      handleBoardTrigger: function (item) {
+        let _t = this
+        _t.currentBoard = item
+        // 获取当前分类下的应用列表
+        _t.getApplicationList()
       }
     },
     created: function () {
