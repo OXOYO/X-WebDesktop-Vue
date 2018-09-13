22cb7eb87f8783c8819496c52309ed3a0f4ca30b
OXOYO <zmn2007.hi@163.com>
2018-09-13 17:31:34 +0800

    完善GoFire浏览器应用。

--- src/apps/GoFire/components/TabPage.vue
+++ src/apps/GoFire/components/TabPage.vue
@@ -25,7 +25,7 @@
         z-index: 1;
 
         &.active {
-          background: #ffffff;
+          background: #ffffff !important;
           color: #3C4043;
         }
 
@@ -36,18 +36,19 @@
           text-align: center;
           flex: none;
           padding: 0;
+          margin-left: 5px;
           background: transparent;
           line-height: initial;
           color: #ffffff;
 
           &:hover {
-            color: #f5f7f9;
+            color: #eeeeee;
             background: transparent;
           }
         }
 
         &:hover {
-          background: #f5f7f9;
+          background: #eeeeee;
         }
 
         .title {
@@ -85,6 +86,11 @@
         font-size: 20px;
         text-align: center;
         vertical-align: middle;
+        border-radius: 50%;
+
+        &:hover {
+          background: #eeeeee;
+        }
       }
       .input {
         display: inline-block;
@@ -103,8 +109,8 @@
       <div
         v-for="(item, key) in tabPageMap"
         :key="key"
-        :class="{ 'tab-bar-item': true, 'active': key === currentTabIndex }"
-        @click.stop.prevent="handleAction('tabActive', key)"
+        :class="{ 'tab-bar-item': true, 'active': key + '' === currentTabIndex + '' }"
+        @click.left.stop.prevent="handleAction('tabActive', key)"
       >
         <div class="title">{{ item.title }}</div>
         <div class="close" @click.stop.prevent="handleAction('tabClose', key)">
@@ -118,24 +124,31 @@
     </div>
     <div class="tab-menu">
       <!-- 后退 -->
-      <div class="tool-item">
+      <div class="tool-item" @click.stop.prevent="handleAction('backward')">
         <Icon type="android-arrow-back"></Icon>
       </div>
       <!-- 前进 -->
-      <div class="tool-item">
+      <div class="tool-item" @click.stop.prevent="handleAction('forward')">
         <Icon type="android-arrow-forward"></Icon>
       </div>
       <!-- 刷新 -->
-      <div class="tool-item">
+      <div class="tool-item" @click.stop.prevent="handleAction('refresh')">
         <Icon type="android-refresh"></Icon>
       </div>
       <!-- 首页 -->
-      <div class="tool-item">
+      <div class="tool-item" @click.stop.prevent="handleAction('home')">
         <Icon type="android-home"></Icon>
       </div>
       <!-- 地址栏 -->
       <div class="input">
-        <Input v-model="currentTab.url" placeholder="请输入一个地址" style="width: 300px" @on-enter="handleAction('site')"></Input>
+        <Input
+          v-model="urlMap[currentTabIndex]"
+          placeholder="请输入一个地址，回车访问"
+          style="width: 300px"
+          :autofocus="true"
+          autocomplete="on"
+          @on-enter="handleAction('visit')"
+        ></Input>
       </div>
     </div>
   </div>
@@ -148,14 +161,23 @@
 
   export default {
     name: 'TabPage',
+    data () {
+      return {
+        urlMap: {}
+      }
+    },
     computed: {
       ...mapState('Apps/' + moduleName, {
         tabPageMap: state => state.tabPageMap,
-        currentTabIndex: state => state.currentTabIndex
-      }),
-      currentTab: function () {
+        currentTabIndex: state => state.currentTabIndex,
+        homePage: state => state.homePage
+      })
+    },
+    watch: {
+      currentTabIndex: function (val) {
         let _t = this
-        return JSON.parse(JSON.stringify(_t.tabPageMap[_t.currentTabIndex]))
+        _t.currentTab = JSON.parse(JSON.stringify(_t.tabPageMap[val]))
+        _t.urlMap[val] = _t.urlMap[val] || _t.currentTab.url
       }
     },
     methods: {
@@ -167,25 +189,60 @@
             _t.$store.commit('Apps/GoFire/tab/active', val)
             break
           case 'tabClose':
+            // 删除相应tab的url
+            delete _t.urlMap[val]
             // 关闭tab页
             _t.$store.commit('Apps/GoFire/tab/close', val)
             break
           case 'newTab':
+            let tabIndex = parseInt(Object.keys(_t.tabPageMap)[Object.keys(_t.tabPageMap).length - 1 > 0 ? Object.keys(_t.tabPageMap).length - 1 : 0]) + 1 + ''
+            _t.urlMap[tabIndex] = ''
             // 新建tab页
             _t.$store.commit('Apps/GoFire/tab/add', val)
             break
-          case 'site':
-            // TODO 处理url
+          case 'visit':
+              // 新建tab页
+            _t.$store.commit('Apps/GoFire/tab/update', {
+              index: _t.currentTabIndex,
+              info: {
+                ..._t.currentTab,
+                url: _t.urlMap[_t.currentTabIndex].replace(/^(https|http|ftp|rtsp|mms)?:?\/?\/?/i, 'http://')
+              }
+            })
+            break
+          case 'backward':
+            // TODO 浏览器后退
+            break
+          case 'forward':
+            // TODO 浏览器前进
+            break
+          case 'refresh':
+            if (_t.currentTab && _t.currentTab.url) {
+              // 刷新tab页
+              _t.$utils.bus.$emit('Apps/GoFire/tab/refresh', {
+                index: _t.currentTabIndex,
+                info: {
+                  ..._t.currentTab
+                }
+              })
+            }
+            break
+          case 'home':
             // 新建tab页
             _t.$store.commit('Apps/GoFire/tab/update', {
               index: _t.currentTabIndex,
               info: {
-                ..._t.currentTab
+                ..._t.homePage
               }
             })
             break
         }
       }
+    },
+    created: function () {
+      let _t = this
+      _t.currentTab = JSON.parse(JSON.stringify(_t.tabPageMap[_t.currentTabIndex]))
+      _t.urlMap[_t.currentTabIndex] = _t.currentTab.url
     }
   }
 </script>
--- src/apps/GoFire/store/state.js
+++ src/apps/GoFire/store/state.js
@@ -5,24 +5,18 @@
 export default {
   // 标签页列表
   tabPageMap: {
-    0: {
+    '0': {
       name: 'home',
       title: 'baidu',
-      url: '//www.baidu.com',
+      url: 'http://www.baidu.com',
       index: 0
-    },
-    1: {
-      name: 'hao123',
-      title: 'hao123',
-      url: 'https://hao123.com',
-      index: 1
     }
   },
   currentTabIndex: '0',
   homePage: {
     name: 'home',
     title: 'baidu',
-    url: '//www.baidu.com',
+    url: 'http://www.baidu.com',
     index: 0
   },
   newTab: {
