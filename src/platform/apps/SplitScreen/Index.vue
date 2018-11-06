5f391a4780a75d8f3f1420f4cb18e40f8561f610
OXOYO <zmn2007.hi@163.com>
2018-11-06 10:40:54 +0800

    优化分屏样式。

--- src/platform/apps/SplitScreen/Index.vue
+++ src/platform/apps/SplitScreen/Index.vue
@@ -17,7 +17,7 @@
       margin: 10px;
       background: transparent;
       border: 1px solid rgba(255, 255, 255, 0.1);
-      box-shadow: 0 0 5px 5px rgba(0, 0, 0, .2);
+      box-shadow: 0 0 5px 5px rgba(0, 0, 0, .5);
     }
     .ripple {
       position: absolute;
