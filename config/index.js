18571cd28ba3a10f82499075de2a774b07e7a105
OXOYO <zmn2007.hi@163.com>
2025-12-26 17:24:14 +0800

    更新生产根路径

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/X-WebDesktop-Vue/',
+    assetsPublicPath: '/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
