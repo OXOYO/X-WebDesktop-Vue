1cfbf22779db95f8393dd4e76f5f828c2a33f65b
OXOYO <zmn2007.hi@163.com>
2025-12-27 12:43:22 +0800

    更新代理配置

--- config/index.js
+++ config/index.js
@@ -27,7 +27,16 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/x-webdesktop-api': {
+        target: 'https://api.oxoyo.co',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          '^/x-webdesktop-api': '/x-webdesktop-api'
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
